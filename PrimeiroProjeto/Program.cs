dd1232f4fbf4746b8bc0de4cf1351984db9cc5f5
Lucas Lodi Kreusberg <kreusberglucas@gmail.com>
2023-09-26 14:59:42 -0300

    creating functions

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -1,4 +1,29 @@
 ﻿// Screen Sound
 String mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
 
-Console.WriteLine(mensagemDeBoasVindas);
+void ExibirMensagemDeBoasVindas()
+{
+    Console.WriteLine(@"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
+
+    Console.WriteLine(mensagemDeBoasVindas);
+}
+
+void ExibirOpcoesDoMenu()
+{
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("\nDigite 2 para mostrar todas as bandas");
+    Console.WriteLine("\nDigite 3 para avaliar uma banda");
+    Console.WriteLine("\nDigite 4 para exibir a média de uma banda");
+    Console.WriteLine("\nDigite -1 para sair ");
+}
+
+ExibirMensagemDeBoasVindas();
+ExibirOpcoesDoMenu();
+
