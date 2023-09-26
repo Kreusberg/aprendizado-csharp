9493459d5604f27ed992fcc56c7498e289bcf611
Lucas Lodi Kreusberg <kreusberglucas@gmail.com>
2023-09-26 15:29:08 -0300

    imcrementing function

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -18,12 +18,38 @@ void ExibirMensagemDeBoasVindas()
 void ExibirOpcoesDoMenu()
 {
     Console.WriteLine("\nDigite 1 para registrar uma banda");
-    Console.WriteLine("\nDigite 2 para mostrar todas as bandas");
-    Console.WriteLine("\nDigite 3 para avaliar uma banda");
-    Console.WriteLine("\nDigite 4 para exibir a média de uma banda");
-    Console.WriteLine("\nDigite -1 para sair ");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair ");
+
+    Console.Write("\nDigite a sua opção: ");
+    String opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    switch (opcaoEscolhidaNumerica)
+    {
+        case 1:
+            Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 2:
+            Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 3:
+            Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 4:
+            Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case -1:
+            Console.WriteLine("Tchau tchau :)");
+            break;
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+
 }
 
 ExibirMensagemDeBoasVindas();
 ExibirOpcoesDoMenu();
-
